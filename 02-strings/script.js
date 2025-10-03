03ecd1bcc6b8e68ef3d624dd8b782d8e38b7b7ab
akthernahela <nahela00@gmail.com>
2025-10-03 14:33:31 +0200

    Add the second sequence content

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,10 @@ console.log(myName);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 23;
+const text = 'Hello everyone';
+const bool = 'false';
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
